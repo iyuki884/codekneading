3e28dcfa3a0789cd5b241be24f1f1d63e1e94f07
iyuki <iyuki884@gmail.com>
2018-10-28 20:48:17 +0900

    プロフィールにSNSリンクを追加

--- docs/.vuepress/components/Profile.vue
+++ docs/.vuepress/components/Profile.vue
@@ -8,12 +8,20 @@
           <img src="/img/profile_icon.jpg" alt="avatar">
         </v-avatar>
         <v-card-text>
-        <div class="name">iyuki</div>
-        <div class="property">
-          ソフトウェア開発エンジニア<br/>
-          パンとコーヒーのお店を開業するのが目標<br/>
-          両立した働き方を実現するために転職活動中<br/>
-        </div>
+          <div class="name">iyuki</div>
+          <div class="sns-link">
+            <a href="https://github.com/iyuki884" target="_blank">
+              <v-icon color="black" >mdi-github-circle</v-icon>
+            </a>
+            <a href="https://twitter.com/iyuki884" target="_blank">
+              <v-icon color="blue">mdi-twitter-circle</v-icon>
+            </a>
+          </div>
+          <div class="property">
+            ソフトウェア開発エンジニア<br/>
+            パンとコーヒーのお店を開業するのが目標<br/>
+            両立した働き方を実現するために転職活動中<br/>
+          </div>
         </v-card-text>
       </v-card>
     </v-flex>
--- docs/.vuepress/theme/enhanceApp.js
+++ docs/.vuepress/theme/enhanceApp.js
@@ -1,5 +1,6 @@
 import Vuetify from '../../../node_modules/vuetify'
 import '../../../node_modules/vuetify/dist/vuetify.min.css'
+import '../../../node_modules/@mdi/font/css/materialdesignicons.css'
 
 export default ({
   Vue,
