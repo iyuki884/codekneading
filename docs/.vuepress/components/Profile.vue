edba5b05a108444994023af0e19b157c7e9a47f8
iyuki <iyuki884@gmail.com>
2018-10-28 19:30:22 +0900

    プロフィールを追加

---
+++ docs/.vuepress/components/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-flex xs-12>
+      <v-card class="text-xs-center">
+        <v-avatar
+          size=140px
+        >
+          <img src="/img/profile_icon.jpg" alt="avatar">
+        </v-avatar>
+        <v-card-text>
+        <div class="name">iyuki</div>
+        <div class="property">
+          ソフトウェア開発エンジニア<br/>
+          パンとコーヒーのお店を開業するのが目標<br/>
+          両立した働き方を実現するために転職活動中<br/>
+        </div>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-container>
+</template>
+
+<style scoped>
+.name {
+  font-weight: bold;
+}
+
+.property {
+  font-size: 13px;
+}
+</style>
--- docs/README.md
+++ docs/README.md
@@ -2,5 +2,9 @@
 home: true
 ---
 ## 最新記事
-
 <PostList />
+
+<v-divider class="my-5" />
+
+## プロフィール
+<Profile />
